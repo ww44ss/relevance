fd662a19f2154b6c2651b5a162a976d3226a4d65
Winston Saunders <winstonasaunders@gmail.com>
2015-05-22 14:23:18 -0700

    some updates

--- reader.R
+++ reader.R
@@ -1,11 +1,22 @@
 library(readr)
 
 # Use readr to read in the training and test data
-train = read_csv("../input/train.csv")
-test  = read_csv("../input/test.csv")
+train = read_csv("./relevance/train.csv")
+test  = read_csv("./relevance/test.csv")
 
 # Print a sample of the training data
 print(head(train))
 
 # Now it's yours to take from here!
 summary(train)
+train[1:4,"query"]
+train[1:4, "product_title"]
+train[]
+hist(train$median_relevance)
+hist(train$relevance_variance)
+
+test[1,]
+test[1:4,"query"]
+test[1:4,"product_title"]
+test[1:4,"product_description"]
+
